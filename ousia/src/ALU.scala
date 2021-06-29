a21a901c22da81676b22ba7bf65c728d38729d50
sinofp <sinofp@tuta.io>
2021-06-29 05:52:07 +0000

    Add ALU & AMOALU

---
+++ ousia/src/ALU.scala
@@ -0,0 +1,54 @@
+import chipsalliance.rocketchip.config.Parameters
+import chisel3._
+import chisel3.util.log2Up
+
+class ALUGeneric(ops: Seq[(UInt, (UInt, UInt) => UInt)])(implicit p: Parameters) extends CoreModule {
+  val io = IO(new Bundle() {
+    val in1 = Input(UInt(xLen.W))
+    val in2 = Input(UInt(xLen.W))
+    val fn  = Input(UInt(log2Up(ops.size).W))
+    val out = Output(UInt(xLen.W))
+  })
+
+  val result = {
+    val _ops = ops.map { case (op, f) => (op, f(io.in1, io.in2)) }
+    util.MuxLookup(io.fn, 0.U, _ops).asUInt
+  }
+
+  io.out := result(xLen - 1, 0)
+}
+
+class ALU(implicit p: Parameters)
+    extends ALUGeneric({
+      import Consts._
+      Seq(
+        ALU_ADD  -> ((a: UInt, b: UInt) => a + b),
+        ALU_SL   -> ((a: UInt, b: UInt) => a << b(4, 0)),
+        ALU_SEQ  -> ((a: UInt, b: UInt) => a === b),
+        ALU_SNE  -> ((a: UInt, b: UInt) => a =/= b),
+        ALU_XOR  -> ((a: UInt, b: UInt) => a ^ b),
+        ALU_SR   -> ((a: UInt, b: UInt) => a >> b),
+        ALU_OR   -> ((a: UInt, b: UInt) => a | b),
+        ALU_AND  -> ((a: UInt, b: UInt) => a & b),
+        ALU_SUB  -> ((a: UInt, b: UInt) => a - b),
+        ALU_SRA  -> ((a: UInt, b: UInt) => (a.asSInt >> b(4, 0)).asUInt),
+        ALU_SLT  -> ((a: UInt, b: UInt) => a.asSInt < b.asSInt),
+        ALU_SGE  -> ((a: UInt, b: UInt) => a.asSInt >= b.asSInt),
+        ALU_SLTU -> ((a: UInt, b: UInt) => a < b),
+        ALU_SGEU -> ((a: UInt, b: UInt) => a >= b),
+      )
+    })
+
+class AMOALU(implicit p: Parameters)
+    extends ALUGeneric({
+      import Consts._
+      Seq(
+        ALU_ADD  -> ((a: UInt, b: UInt) => a + b),
+        ALU_XOR  -> ((a: UInt, b: UInt) => a ^ b),
+        ALU_OR   -> ((a: UInt, b: UInt) => a | b),
+        ALU_AND  -> ((a: UInt, b: UInt) => a & b),
+        ALU_SLT  -> ((a: UInt, b: UInt) => a.asSInt < b.asSInt),
+        ALU_SLTU -> ((a: UInt, b: UInt) => a < b),
+        ALU_SUB  -> ((_: UInt, b: UInt) => b),// SWAP
+      )
+    })
---
+++ ousia/src/Consts.scala
@@ -0,0 +1,45 @@
+import chisel3._
+import chisel3.util.BitPat
+
+object Consts {
+  val NOP_INST = 0x13.U(32.W)
+
+  val SZ_SEL_ALU1 = 2
+  val A1_ZERO     = 0.U
+  val A1_RS1      = 1.U
+  val A1_PC       = 2.U
+
+  val SZ_SEL_ALU2 = 0
+  val A2_ZERO     = 0.U
+  val A2_RS2      = 1.U
+  val A2_IMM      = 2.U
+
+  val SZ_ALU_FN = 4
+  val ALU_X     = BitPat("b????")
+  val ALU_ADD   = 0.U
+  val ALU_SL    = 1.U
+  val ALU_SEQ   = 2.U
+  val ALU_SNE   = 3.U
+  val ALU_XOR   = 4.U
+  val ALU_SR    = 5.U
+  val ALU_OR    = 6.U
+  val ALU_AND   = 7.U
+  val ALU_SUB   = 10.U
+  val ALU_SRA   = 11.U
+  val ALU_SLT   = 12.U
+  val ALU_SGE   = 13.U
+  val ALU_SLTU  = 14.U
+  val ALU_SGEU  = 15.U
+
+  val ALU_DIVU   = ALU_SR
+  val ALU_REM    = ALU_OR
+  val ALU_REMU   = ALU_AND
+  val ALU_MUL    = ALU_ADD
+  val ALU_MULH   = ALU_SL
+  val ALU_MULHSU = ALU_SEQ
+  val ALU_MULHU  = ALU_SNE
+
+  val X = BitPat("b?")
+  val N = BitPat("b0")
+  val Y = BitPat("b1")
+}
